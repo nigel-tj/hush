feb09782342ecea12963ddf211370b49adddec43
Nigel <hypej.10@gmail.com>
2021-04-12 16:11:24 +0200

    hush.io: fix ui for recording

--- src/app/pages/hushout/hushout.page.ts
+++ src/app/pages/hushout/hushout.page.ts
@@ -21,6 +21,29 @@ const MEDIA_FOLDER_NAME = 'my_media';
 })
 export class HushoutPage implements OnInit {
   files = [];
+  recording: boolean = false;
+  filePath: string;
+  fileName: string;
+  audio: MediaObject;
+  audioList: any[] = [];
+
+  title: any;
+  artist: any;
+  image: string = 'assets/album_art.jpg';
+  filename: any = 'Baba O`reily';
+  duration: any = -1;
+  curr_playing_file: MediaObject;
+  storageDirectory: any;
+  play_The_track: string = "/android_asset/public/assets/GOT.mp3"; //note this specific url format is used in android only
+  position: any = 0;
+  get_position_interval: any;
+  is_playing = false;
+  is_in_play = false;
+  is_recording = false;
+  is_ready = false;
+  get_duration_interval: any;
+  display_position: any = '00:00';
+  display_duration: any = '00:00';          
 
   constructor(
     private imagePicker: ImagePicker,
@@ -36,8 +59,7 @@ export class HushoutPage implements OnInit {
   ngOnInit() {
     this.plt.ready().then(() => {
       let path = this.file.dataDirectory;
-      this.file.checkDir(path, MEDIA_FOLDER_NAME).then(
-        () => {
+      this.file.checkDir(path, MEDIA_FOLDER_NAME).then(() => {
           this.loadFiles();
         },
         err => {
@@ -55,93 +77,23 @@ export class HushoutPage implements OnInit {
       err => console.log('error loading files: ', err)
     );
   }
-
-  async selectMedia() {
-    const actionSheet = await this.actionSheetController.create({
-      header: 'What would you like to add?',
-      buttons: [
-        {
-          text: 'Capture Image',
-          handler: () => {
-            this.captureImage();
-          }
-        },
-        {
-          text: 'Record Video',
-          handler: () => {
-            this.recordVideo();
-          }
-        },
-        {
-          text: 'Record Audio',
-          handler: () => {
-            this.recordAudio();
-          }
-        },
-        {
-          text: 'Load multiple',
-          handler: () => {
-            this.pickImages();
-          }
-        },
-        {
-          text: 'Cancel',
-          role: 'cancel'
-        }
-      ]
-    });
-    await actionSheet.present();
-  }
- 
-  pickImages() {
-    this.imagePicker.getPictures({}).then(
-      results => {
-        for (var i = 0; i < results.length; i++) {
-          this.copyFileToLocalDir(results[i]);
-        }
-      }
-    );
- 
-    // If you get problems on Android, try to ask for Permission first
-    // this.imagePicker.requestReadPermission().then(result => {
-    //   console.log('requestReadPermission: ', result);
-    //   this.selectMultiple();
-    // });
-  }
- 
-  captureImage() {
-    this.mediaCapture.captureImage().then(
-      (data: MediaFile[]) => {
-        if (data.length > 0) {
-          this.copyFileToLocalDir(data[0].fullPath);
-        }
-      },
-      (err: CaptureError) => console.error(err)
-    );
-  }
- 
+  
   recordAudio() {
-    this.mediaCapture.captureAudio().then(
-      (data: MediaFile[]) => {
-        if (data.length > 0) {
-          this.copyFileToLocalDir(data[0].fullPath);
-        }
-      },
-      (err: CaptureError) => console.error(err)
-    );
+
+    if (this.plt.is('ios')) {
+      this.fileName = 'record'+new Date().getDate()+new Date().getMonth()+new Date().getFullYear()+new Date().getHours()+new Date().getMinutes()+new Date().getSeconds()+'.3gp';
+      this.filePath = this.file.documentsDirectory.replace(/file:\/\//g, '') + this.fileName;
+      this.audio = this.media.create(this.filePath);
+    } else if (this.plt.is('android')) {
+      this.fileName = 'record'+new Date().getDate()+new Date().getMonth()+new Date().getFullYear()+new Date().getHours()+new Date().getMinutes()+new Date().getSeconds()+'.3gp';
+      this.filePath = this.file.externalDataDirectory.replace(/file:\/\//g, '') + this.fileName;
+      this.audio = this.media.create(this.filePath);
+    }
+    this.audio.startRecord();
+    this.is_recording = true;
+
   }
  
-  recordVideo() {
-    this.mediaCapture.captureVideo().then(
-      (data: MediaFile[]) => {
-        if (data.length > 0) {
-          this.copyFileToLocalDir(data[0].fullPath);
-        }
-      },
-      (err: CaptureError) => console.error(err)
-    );
-  }
-
   copyFileToLocalDir(fullPath) {
     let myPath = fullPath;
     // Make sure we copy from the right location
@@ -187,6 +139,135 @@ export class HushoutPage implements OnInit {
     this.file.removeFile(path, f.name).then(() => {
       this.loadFiles();
     }, err => console.log('error remove: ', err));
+  }
+
+
+
+  prepareAudioFile() {
+    this.plt.ready().then((res) => {
+      this.getDuration();
+    });
+  }
+
+  getDuration() {
+    this.curr_playing_file = this.media.create(this.play_The_track);
+    // on occassions, the plugin only gives duration of the file if the file is played
+    // at least once
+    this.curr_playing_file.play();
+
+    this.curr_playing_file.setVolume(0.0);  // you don't want users to notice that you are playing the file
+    const self = this;
+    // The plugin does not give the correct duration on playback start
+    // Need to check for duration repeatedly
+    let temp_duration = self.duration;
+    this.get_duration_interval = setInterval(() => {
+      if (self.duration === -1 || !self.duration) {
+        self.duration = ~~(self.curr_playing_file.getDuration());  // make it an integer
+      } else {
+        if (self.duration !== temp_duration) {
+          temp_duration = self.duration;
+        }
+        else {
+          self.curr_playing_file.stop();
+          self.curr_playing_file.release();
+
+          clearInterval(self.get_duration_interval);
+          this.display_duration = this.toHHMMSS(self.duration);
+          self.setToPlayback();
+        }
+      }
+    }, 100);
+  }
+
+  setToPlayback() {
+    this.curr_playing_file = this.media.create(this.play_The_track);
+    this.curr_playing_file.onStatusUpdate.subscribe(status => {
+      switch (status) {
+        case 1:
+          break;
+        case 2:   // 2: playing
+          this.is_playing = true;
+          break;
+        case 3:   // 3: pause
+          this.is_playing = false;
+          break;
+        case 4:   // 4: stop
+        default:
+          this.is_playing = false;
+          break;
+      }
+    });
+    this.is_ready = true;
+    this.getAndSetCurrentAudioPosition();
+  }
+
+  getAndSetCurrentAudioPosition() {
+    const diff = 1;
+    const self = this;
+    this.get_position_interval = setInterval(() => {
+      const last_position = self.position;
+      self.curr_playing_file.getCurrentPosition().then((position) => {
+        if (position >= 0 && position < self.duration) {
+          if (Math.abs(last_position - position) >= diff) {
+            // set position
+            self.curr_playing_file.seekTo(last_position * 1000);
+
+          } else {
+            // update position for display
+            self.position = position;
+            this.display_position = this.toHHMMSS(self.position);
+          }
+        } else if (position >= self.duration) {
+          self.stop();
+          self.setToPlayback();
+        }
+      });
+    }, 100);
+  }
+
+  play() {
+    this.curr_playing_file.play();
+  }
+
+  pause() {
+    this.curr_playing_file.pause();
+  }
+
+  stop() {
+    this.curr_playing_file.stop();
+    this.curr_playing_file.release();
+    clearInterval(this.get_position_interval);
+    this.position = 0;
+  }
+
+  controlSeconds(action) {
+    const step = 5;
+    const numberRange = this.position;
+    switch (action) {
+      case 'back':
+        this.position = numberRange < step ? 0.001 : numberRange - step;
+        break;
+      case 'forward':
+        this.position = numberRange + step < this.duration ? numberRange + step : this.duration;
+        break;
+      default:
+        break;
+    }
+  }
+
+  ngOnDestroy() {
+    this.stop();
+  }
+
+  toHHMMSS(secs) {
+    var sec_num = parseInt(secs, 10)
+    var minutes = Math.floor(sec_num / 60) % 60
+    var seconds = sec_num % 60
+
+    return [minutes, seconds]
+      .map(v => v < 10 ? "0" + v : v)
+      .filter((v, i) => v !== "00" || i >= 0)
+      .join(":")
   }
 
 }
